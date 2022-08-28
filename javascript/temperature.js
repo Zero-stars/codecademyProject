54e6fa78d853543c699be03d287b49816127b215
Stephen Lin <81601143+Zero-stars@users.noreply.github.com>
2022-08-28 12:11:43 -0400

    Update temperature.js

--- javascript/temperature.js
+++ javascript/temperature.js
@@ -1,18 +1,18 @@
 //Tmperature given in kelvin
-const kelvin = 0
+const kelvin = 0;
 
 //celsius is 273 degree less than kelvin
-const celsius = kelvin - 273
+const celsius = kelvin - 273;
 
 //Convert celsius to fahrenheit
-let fahrenheit = celsius *(9/5)+32
+let fahrenheit = celsius *(9/5)+32;
 //Round the fahrenheit down
-fahrenheit = Math.floor(fahrenheit)
+fahrenheit = Math.floor(fahrenheit);
 
 //Convert celsius to Newton
-let newton = celsius*(33/100)
+let newton = celsius*(33/100);
 //Round Newton down 
-newton = Math.floor(newton)
+newton = Math.floor(newton);
 
-console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`)
-console.log(`The temperature is ${newton} N.`)
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+console.log(`The temperature is ${newton} N.`);
